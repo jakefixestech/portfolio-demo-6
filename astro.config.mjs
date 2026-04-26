f9bedadfd1127d9abb57861e2a554c8854f11c65
Jake <jake@jakefixestech.com>
2026-04-26 16:59:38 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ironsidebarber.co',
-  compressHTML: true,
+  site: 'https://demo6.jakefixestech.com/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
